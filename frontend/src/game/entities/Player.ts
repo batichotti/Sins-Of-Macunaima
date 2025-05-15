2bc2206edcad309d7b2fe3cea3496b79f654c8c9
Pedro N <itiropedro@gmail.com>
2025-05-15 16:54:39 -0300

    animação jogador

--- frontend/src/game/entities/Player.ts
+++ frontend/src/game/entities/Player.ts
@@ -1,6 +1,20 @@
 import { BaseCharacterStats } from "@/game/components/Constants";
 import { PlayerData } from "@/game/components/Types"
 
+// mover para outro canto
+
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
+
+const frameConfig = {
+    frameRate: 4,
+    repeat: -1,
+}
+
 export default class Player {
     playerData!:PlayerData;
     character!:Character;
@@ -15,6 +29,7 @@ class Character {
     scene!: Phaser.Scene;
     sprite!:Phaser.Physics.Arcade.Sprite;
     position: Phaser.Math.Vector2;
+    direction: Direction;
     life = BaseCharacterStats.life;
     speed = BaseCharacterStats.speed;
     scale = BaseCharacterStats.scale;
@@ -32,13 +47,78 @@ class Character {
             true
         ).setDepth(100);
         this.sprite.setFrame(0);
+        this.sprite.anims.create(
+            {
+                key: 'up',
+                frames: this.sprite.anims.generateFrameNumbers(key, {  frames: [13, 14] }),
+                ...frameConfig
+            }
+        );
+        this.sprite.anims.create(
+            {
+                key: 'down',
+                frames: this.sprite.anims.generateFrameNumbers(key, {  frames: [1, 2] }),
+                ...frameConfig
+            }
+        );
+
+        this.sprite.anims.create(
+            {
+                key: 'left',
+                frames: this.sprite.anims.generateFrameNumbers(key, { frames: [9, 10] }),
+                ...frameConfig
+            }
+        );
+
+        this.sprite.anims.create(
+            {
+                key: 'right',
+                frames: this.sprite.anims.generateFrameNumbers(key, { frames: [5, 6] }),
+                ...frameConfig
+            }
+        );
     }
 
     public updateMovement(velocity: Phaser.Math.Vector2): void {
         const normalized = velocity.normalize().scale(this.speed);
-        if(velocity.x > 0) this.sprite.setFrame(1);
-        else if(velocity.x < 0) this.sprite.setFrame(2);
-        else this.sprite.setFrame(0);
+        
+        if(velocity.x > 0) {
+            this.direction = Direction.RIGHT;
+            this.sprite.play('right', true);
+        }
+        else if(velocity.x < 0) {
+            this.direction = Direction.LEFT;
+            this.sprite.play('left', true);
+        }
+        else if(velocity.y > 0) {
+            this.direction = Direction.DOWN;
+            this.sprite.play('down', true);
+        }
+        else if(velocity.y < 0) {
+            this.direction = Direction.UP;
+            this.sprite.play('up', true);
+        }
+
+        else if(velocity.x == 0 && velocity.y == 0) {
+            switch(this.direction) {
+                case Direction.UP:
+                    this.sprite.setFrame(12);
+                    break;
+                case Direction.DOWN:
+                    this.sprite.setFrame(0);
+                    break;
+                case Direction.LEFT:
+                    this.sprite.setFrame(8);
+                    break;
+                case Direction.RIGHT:
+                    this.sprite.setFrame(4);
+                    break;
+                default:
+                    this.sprite.setFrame(0);
+            }
+        }
+
+
         this.sprite.setVelocity(normalized.x, normalized.y);
     }
 }
