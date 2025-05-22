73f499533285c8898dc4e227af07c0c307285e75
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-05-22 15:02:50 -0300

    TypeORM added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TesteModule } from './teste/teste.module';
-import { TesteModule } from './teste/teste.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TesteModule],
+  
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'nestdb',
+      autoLoadEntities: true,
+      synchronize: true,}),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
