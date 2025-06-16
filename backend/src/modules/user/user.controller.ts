4a3fd9a640d57ff5c3de6d5387a5c269d9e7ded8
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-06-16 08:12:58 -0300

    Dependency Injection

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,24 +1,27 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+    constructor(private userService: UserService){}
+
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
-        return 'All users';
+        return this.userService.findAll(sort);
     }
-
+    
     @Get('top')
     findTopScorers() {
-        return 'Top Scorers';
+        return this.userService.findTopScorers();
     }
-
+    
     @Get(':id') // URL parameter
     findOne(@Param() id: string){
-        return 'One user';
+        return this.userService.findOne(id);
     }
-
+    
     @Post()
     create(@Body() input: any) {
-        return 'New user';
+        return this.userService.create(input);
     }
 }
--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    findAll(sort: 'asc' | 'desc' = 'desc') {
+        return 'All users';
+    }
+
+    findTopScorers() {
+        return 'Top Scorers';
+    }
+
+    findOne(id: string) {
+        return 'One user';
+    }
+
+    create(input: any) {
+        return 'New user';
+    }
+}
