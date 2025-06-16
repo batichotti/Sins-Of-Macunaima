42e4122f5c77fe3c6efe5b68082966506ecdede4
Pedro N <itiropedro@gmail.com>
2025-06-16 12:26:47 -0300

    Update EventBus.ts

--- frontend/src/game/core/EventBus.ts
+++ frontend/src/game/core/EventBus.ts
@@ -1,5 +1,5 @@
 import { Events } from 'phaser';
-import { GameEvents, GameEventsPayloads } from '../types';
+import {  GameEventsPayloads } from '../types';
 
 /**
  * Usado para emitir eventos entre componentes React e cenas Phaser.
@@ -16,7 +16,6 @@ export const EventBus = new Events.EventEmitter();
 export class EventManager {
     private static instance: EventManager;
     private emitter: Phaser.Events.EventEmitter;
-    private listeners: Map<GameEvents, Set<EventListener<object>>> = new Map();
 
     private constructor() {
       this.emitter = new Phaser.Events.EventEmitter();
@@ -34,26 +33,10 @@ export class EventManager {
     }
 
     public on<T extends keyof GameEventsPayloads>(event: T, callback: (payload: GameEventsPayloads[T]) => void, context?: object): void {
-      const listener: EventListener<T> = { event: event, callback: callback, context: context }
-      if (!this.listeners.has(event as GameEvents)) {
-        this.listeners.set(event as GameEvents, new Set());
-      }
-      this.listeners.get(event as GameEvents)!.add(listener);
-
       this.emitter.on(event as string, callback, context);
     }
 
     public off<T extends keyof GameEventsPayloads>(event: T, callback?: (data: GameEventsPayloads[T]) => void,context?: object): void {
-      if (callback) {
-        this.emitter.off(event as string, callback, context);
-
-        const listeners = this.listeners.get(event as GameEvents);
-        if (listeners) {
-          listeners.forEach(listener => { if (listener.callback === callback && listener.context === context) listeners.delete(listener) });
-        }
-      } else {
-        this.emitter.removeAllListeners(event as string);
-        this.listeners.delete(event as GameEvents);
-      }
+      this.emitter.off(event as string, callback, context);
     }
 }
